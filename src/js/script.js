3080c72949311993cb907a83a9eb81179fc4930a
Banan <react.vue.stdnt@mail.ru>
2020-04-03 05:00:55 +0300

    made slick slaider with jquure

--- src/js/script.js
+++ src/js/script.js
@@ -5,10 +5,26 @@ window.addEventListener('DOMContentLoaded', () => {
     // forms_clear();
     // попробуем просто так 
     const inputPhone = document.querySelectorAll('input[name="phone"]');
-    
+
     inputPhone.forEach(item => {
         item.addEventListener('input', () => {
             item.value = item.value.replace(/\D/, '');
         });
+    });
+});
+
+$(document).ready(function () {
+    $('.carousel__iner').slick({
+        speed: 1200,
+        // adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/4-5_section/left.png" alt="x"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/4-5_section/right.png" alt="x"></button>',
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                arrows: false,
+                dots: true
+            }
+        }]
     });
 });
