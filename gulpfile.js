3c0d1f443cf07c6062b1f5fbdd8b9f05c7fe8c5e
Banan <react.vue.stdnt@mail.ru>
2020-04-19 00:56:35 +0300

    Optimization complet, good work!

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,18 @@ const sass = require('gulp-sass');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 // Static server
 gulp.task('server', function() {
 
 		browserSync.init({
 				server: {
-						baseDir: "src"
+						baseDir: "dist"
 				}
-		});
+      });
+      gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
 gulp.task('styles', function() {
@@ -24,14 +27,46 @@ gulp.task('styles', function() {
 						}))
 						// .pipe(autoprefixer())
 						.pipe(cleanCSS({compatibility: 'ie8'}))
-						.pipe(gulp.dest("src/css"))
+						.pipe(gulp.dest("dist/css"))
 						.pipe(browserSync.stream());
 });
 
 gulp.task('watch', function(){
-        gulp.watch("src/sass/**/*.+(sass|scss)", gulp.parallel("styles"));
-        gulp.watch("src/*.js").on("change", browserSync.reload);
-		gulp.watch("src/*.html").on("change", browserSync.reload);
+        gulp.watch("src/sass/**/*.+(sass|scss|css)", gulp.parallel("styles"));
+		  gulp.watch("src/*.html").on("change", gulp.parallel("html"));
 });
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+gulp.task('html', function(){
+   return gulp.src("src/*.html")
+               .pipe(htmlmin({ collapseWhitespace: true }))
+               .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('img', function(){
+   return gulp.src("src/img/**/*")
+               .pipe(imagemin())
+               .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('fonts', function(){
+   return gulp.src("src/fonts/**/*")
+               .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function(){
+   return gulp.src("src/icons/**/*")
+               .pipe(imagemin())
+               .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('js', function(){
+   return gulp.src("src/js/**/*.js")
+               .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('mailer', function(){
+   return gulp.src("src/mailer/**/*")
+               .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'mailer', 'js', 'icons', 'fonts', 'img', 'html'));
